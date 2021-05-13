a77191a20cc13e716662d8be2f8d12d25ee3ad0c
Toomas Tamm <toomastmm@gmail.com>
2021-05-13 19:53:41 +0300

    Add Builder Interface

---
+++ Assets/Scripts/Builder.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBuilder
+{
+    void BuildWall(int x, int y);
+
+    void BuildFloor(int x, int y);
+}
